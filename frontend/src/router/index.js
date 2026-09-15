9f908de08cf69ff2332202a157fcfd891ddb318a
Yaroslav Ognevoy <ya.ognevoy@gmail.com>
2026-09-15 08:54:50 +0300

    feat(frontend): add upload and status routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
 import HomeView from '../views/HomeView.vue'
+import UploadView from '../views/UploadView.vue'
+import StatusView from '../views/StatusView.vue'
 import QueueView from '../views/admin/QueueView.vue'
 import CaseDetailView from '../views/admin/CaseDetailView.vue'
 
@@ -12,10 +15,27 @@ const routes = [
     component: LoginView,
   },
   {
+    path: '/register',
+    name: 'register',
+    component: RegisterView,
+  },
+  {
     path: '/',
     name: 'home',
     component: HomeView,
     meta: { requiresAuth: true },
+  },
+  {
+    path: '/upload',
+    name: 'upload',
+    component: UploadView,
+    meta: { requiresAuth: true, role: 'applicant' },
+  },
+  {
+    path: '/status',
+    name: 'status',
+    component: StatusView,
+    meta: { requiresAuth: true, role: 'applicant' },
   },
   {
     path: '/admin',
